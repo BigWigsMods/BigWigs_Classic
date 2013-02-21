c2d4e52cfa2b839b916a4ca87380dc160faabac2
funkydude <funkydude@users.noreply.github.com>
2013-02-21 16:50:06 +0000

    more API updates

--- BlackwingLair/Razorgore.lua
+++ BlackwingLair/Razorgore.lua
@@ -5,7 +5,7 @@
 local mod, CL = BigWigs:NewBoss("Razorgore the Untamed", 755)
 if not mod then return end
 mod:RegisterEnableMob(12435, 12557) -- Razorgore, Grethok the Controller
-mod.toggleOptions = {14515, "eggs", "stages", "bosskill"}
+mod.toggleOptions = {14515, {23023, "ICON"}, "eggs", "stages", "bosskill"}
 
 local eggs = 0
 
@@ -36,14 +36,17 @@ L = mod:GetLocale()
 function mod:OnBossEnable()
 	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", "EggKill", "pet", "target", "focus")
 	self:Log("SPELL_AURA_APPLIED", "DominateMind", 14515)
-	self:Yell("Engage", L["start_trigger"])
+	self:Log("SPELL_AURA_APPLIED", "Conflagration", 23023)
+	self:Log("SPELL_AURA_REMOVED", "ConflagrationOver", 23023)
+	self:Log("SPELL_CAST_SUCCESS", "Phase2", 23040)
+	self:Yell("Engage", L.start_trigger)
 	self:AddSyncListener("EggKill")
 end
 
 function mod:OnEngage()
-	self:Message("stages", "Urgent", nil, L["start_message"], false)
-	self:Bar("stages", 45, L["start_mob"], "Spell_Holy_PrayerOfHealing")
-	self:DelayedMessage("stages", 40, "Important", L["start_soon"])
+	self:Message("stages", "Urgent", nil, L.start_message, false)
+	self:Bar("stages", 45, L.start_mob, "Spell_Holy_PrayerOfHealing")
+	self:DelayedMessage("stages", 40, "Important", L.start_soon)
 	eggs = 0
 end
 
@@ -59,10 +62,8 @@ do
 	function mod:OnSync(sync)
 		if sync == "EggKill" then
 			eggs = eggs + 1
-			self:Message("eggs", "Positive", nil, L["eggs_message"]:format(eggs), 19873)
-			if eggs == 30 then
-				self:Message("stages", "Important", nil, L["phase2_message"], false)
-				self:Death("Win", 12435) -- Register after p2 to prevent false positives
+			if eggs < 30 then
+				self:Message("eggs", "Positive", nil, L.eggs_message:format(eggs), 19873)
 			end
 		end
 	end
@@ -72,5 +73,21 @@ do
 			self:Sync("EggKill")
 		end
 	end
+end
+
+function mod:Phase2()
+	self:Message("stages", "Important", nil, L.phase2_message, false)
+	self:Death("Win", 12435) -- Register after p2 to prevent false positives
+end
+
+function mod:Conflagration(args)
+	self:TargetMessage(args.spellId, args.destName, "Urgent", "Info")
+	self:TargetBar(args.spellId, 10, args.destName)
+	self:PrimaryIcon(args.spellId, args.destName)
+end
+
+function mod:ConflagrationOver(args)
+	self:StopBar(args.spellId, args.destName)
+	self:PrimaryIcon(args.spellId)
 end
 
--- BlackwingLair/Vaelastrasz.lua
+++ BlackwingLair/Vaelastrasz.lua
@@ -8,22 +8,11 @@ mod:RegisterEnableMob(13020)
 mod.toggleOptions = {{18173, "ICON"}, "bosskill"}
 
 --------------------------------------------------------------------------------
--- Localization
---
-
-local L = mod:NewLocale("enUS", true)
-if L then
-	
-end
-L = mod:GetLocale()
-
---------------------------------------------------------------------------------
 -- Initialization
 --
 
 function mod:OnBossEnable()
-	self:Log("SPELL_AURA_APPLIED", "Adrenaline", 18173, 23620)
-	self:Log("SPELL_AURA_REMOVED", "AdrenalineOver", 18173, 23620)
+	self:Log("SPELL_AURA_APPLIED", "Adrenaline", 18173)
 
 	self:Death("Win", 13020)
 end
@@ -40,9 +29,5 @@ function mod:Adrenaline(args)
 	self:TargetMessage(args.spellId, args.destName, "Attention", "Alarm")
 	self:PrimaryIcon(args.spellId, args.destName)
 	self:TargetBar(args.spellId, 20, args.destName, 67729, args.spellId) -- Explode
-end
-
-function mod:AdrenalineOver(args)
-	self:PrimaryIcon(args.spellId)
 end
 
