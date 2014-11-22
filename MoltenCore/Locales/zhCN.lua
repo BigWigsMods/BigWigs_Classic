cbb630062e037a6afe3953f6c00ce8162975f16b
Adavak <adavak@adavak.com>
2014-11-22 06:44:13 +0000

    - zhCN/zhTW update.

--- MoltenCore/Locales/zhCN.lua
+++ MoltenCore/Locales/zhCN.lua
@@ -1,72 +1,72 @@
 local L = BigWigs:NewBossLocale("Lucifron", "zhCN")
 if not L then return end
 if L then
-	--L.bossName = "Lucifron"
+	L.bossName = "鲁西弗隆"
 
-	L.mc_bar = "控制: %s"
+	L.mc_bar = "控制：%s"
 end
 
 L = BigWigs:NewBossLocale("Magmadar", "zhCN")
 if L then
-	--L.bossName = "Magmadar"
+	L.bossName = "玛格曼达"
 end
 
 L = BigWigs:NewBossLocale("Gehennas", "zhCN")
 if L then
-	--L.bossName = "Gehennas"
+	L.bossName = "基赫纳斯"
 end
 
 L = BigWigs:NewBossLocale("Garr", "zhCN")
 if L then
-	--L.bossName = "Garr"
+	L.bossName = "加尔"
 end
 
 L = BigWigs:NewBossLocale("Baron Geddon", "zhCN")
 if L then
-	--L.bossName = "Baron Geddon"
+	L.bossName = "迦顿男爵"
 end
 
 L = BigWigs:NewBossLocale("Shazzrah", "zhCN")
 if L then
-	--L.bossName = "Shazzrah"
+	L.bossName = "沙斯拉尔"
 end
 
 L = BigWigs:NewBossLocale("Sulfuron Harbinger", "zhCN")
 if L then
-	--L.bossName = "Sulfuron Harbinger"
+	L.bossName = "萨弗隆先驱者"
 end
 
 L = BigWigs:NewBossLocale("Golemagg the Incinerator", "zhCN")
 if L then
-	--L.bossName = "Golemagg the Incinerator"
+	L.bossName = "焚化者古雷曼格"
 end
 
 L = BigWigs:NewBossLocale("Majordomo Executus", "zhCN")
 if L then
-	--L.bossName = "Majordomo Executus"
+	L.bossName = "管理者埃克索图斯"
 
-	L.disabletrigger = "不可能！等一下"
-	--L.power_next = "Next Power"
+	L.disabletrigger = "不……不可能！等一下……我投降！我投降！"
+	L.power_next = "下一能量"
 end
 
 L = BigWigs:NewBossLocale("Ragnaros ", "zhCN")
 if L then
-	--L.bossName = "Ragnaros"
+	L.bossName = "拉格纳罗斯"
 
-	L.submerge_trigger = "出现吧，我的奴仆"
 	L.engage_trigger = "现在轮到你们了"
+	L.submerge_trigger = "出现吧，我的奴仆"
 
-	L.knockback_bar = "群体击退"
 	L.knockback_message = "群体击退！"
+	L.knockback_bar = "群体击退"
 
-	L.emerge = "出现警报"
-	L.emerge_desc = "出现警报"
-	L.emerge_message = "拉格纳罗斯已经激活，将在3分钟后暂时消失并召唤烈焰之子！"
-	L.emerge_bar = "拉格纳罗斯出现"
+	L.submerge = "消失"
+	L.submerge_desc = "当拉格纳罗斯消失时发出警报。"
+	L.submerge_message = "拉格纳罗斯消失90秒！"
+	L.submerge_bar = "消失"
 
-	L.submerge = "消失警报"
-	L.submerge_desc = "拉格纳罗斯消失，儿子出现发出警报"
-	L.submerge_message = "拉格纳罗斯消失90秒。烈焰之子出现！"
-	L.submerge_bar = "拉格纳罗斯消失"
+	L.emerge = "出现"
+	L.emerge_desc = "当拉格纳罗斯出现时发出警报。"
+	L.emerge_message = "拉格纳罗斯已经激活，3分钟后消失！"
+	L.emerge_bar = "出现"
 end
 
--- MoltenCore/Locales/zhTW.lua
+++ MoltenCore/Locales/zhTW.lua
@@ -1,72 +1,72 @@
 local L = BigWigs:NewBossLocale("Lucifron", "zhTW")
 if not L then return end
 if L then
-	--L.bossName = "Lucifron"
+	L.bossName = "魯西弗隆"
 
-	L.mc_bar = "控制: %s"
+	L.mc_bar = "控制：%s"
 end
 
 L = BigWigs:NewBossLocale("Magmadar", "zhTW")
 if L then
-	--L.bossName = "Magmadar"
+	L.bossName = "瑪格曼達"
 end
 
 L = BigWigs:NewBossLocale("Gehennas", "zhTW")
 if L then
-	--L.bossName = "Gehennas"
+	L.bossName = "基赫納斯"
 end
 
 L = BigWigs:NewBossLocale("Garr", "zhTW")
 if L then
-	--L.bossName = "Garr"
+	L.bossName = "加爾"
 end
 
 L = BigWigs:NewBossLocale("Baron Geddon", "zhTW")
 if L then
-	--L.bossName = "Baron Geddon"
+	L.bossName = "迦頓男爵"
 end
 
 L = BigWigs:NewBossLocale("Shazzrah", "zhTW")
 if L then
-	--L.bossName = "Shazzrah"
+	L.bossName = "沙斯拉爾"
 end
 
 L = BigWigs:NewBossLocale("Sulfuron Harbinger", "zhTW")
 if L then
-	--L.bossName = "Sulfuron Harbinger"
+	L.bossName = "薩弗隆先驅者"
 end
 
 L = BigWigs:NewBossLocale("Golemagg the Incinerator", "zhTW")
 if L then
-	--L.bossName = "Golemagg the Incinerator"
+	L.bossName = "『焚化者』古雷曼格"
 end
 
 L = BigWigs:NewBossLocale("Majordomo Executus", "zhTW")
 if L then
-	--L.bossName = "Majordomo Executus"
+	L.bossName = "管理者埃克索圖斯"
 
 	L.disabletrigger = "不……不可能！等一下……我投降！我投降！"
-	--L.power_next = "Next Power"
+	L.power_next = "下一能量"
 end
 
 L = BigWigs:NewBossLocale("Ragnaros ", "zhTW")
 if L then
-	--L.bossName = "Ragnaros"
+	L.bossName = "拉格納羅斯"
 
-	L.submerge_trigger = "出現吧，我的奴僕"
 	L.engage_trigger = "現在輪到你們了"
+	L.submerge_trigger = "出現吧，我的奴僕"
 
 	L.knockback_message = "群體擊退！"
 	L.knockback_bar = "群體擊退"
 
-	L.submerge = "消失警報"
+	L.submerge = "消失"
 	L.submerge_desc = "當拉格納羅斯消失時發出警報"
 	L.submerge_message = "消失 90 秒！ 烈焰之子出現！"
 	L.submerge_bar = "拉格納羅斯消失"
 
-	L.emerge = "出現警報"
-	L.emerge_desc = "當拉格納羅斯出現消失時發出警報"
-	L.emerge_message = "拉格納羅斯已經進入戰鬥，3 分鐘後暫時消失並召喚烈焰之子"
-	L.emerge_bar = "拉格納羅斯出現"
+	L.emerge = "出現"
+	L.emerge_desc = "當拉格納羅斯出現時發出警報。"
+	L.emerge_message = "拉格納羅斯已經進入戰鬥，3分鐘後消失！"
+	L.emerge_bar = "出現"
 end
 
