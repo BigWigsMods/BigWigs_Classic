23b96ce54fd233450734ccc2f065a8f01bd29adf
funkydude <funkydude@users.noreply.github.com>
2023-12-18 19:00:33 +0000

    Onyxia_Classic/Onyxia: Backport missing IDs from wrath, updates, cleanups

--- Onyxia_Classic/Locales/deDE.lua
+++ Onyxia_Classic/Locales/deDE.lua
@@ -1,14 +1,8 @@
 local L = BigWigs:NewBossLocale("Onyxia", "deDE")
 if L then
-	L.phase = "Phasen"
-	L.phase_desc = "Warnt, wenn ein Phasenwechsel bevorsteht."
-	L.phase2_message = "Phase 2 - Onyxia hebt ab!"
-	L.phase3_message = "Phase 3 - Onyxia landet!"
-
 	L.phase1_trigger = "Was für ein Zufall"
 	L.phase2_trigger = "Diese sinnlose Anstrengung langweilt mich"
 	L.phase3_trigger = "Mir scheint, dass Ihr noch eine Lektion braucht"
 
-	L.deepbreath_message = "Tiefer Atem!"
-	L.fear_message = "Furcht!"
+	L.deep_breath = "Tiefer Atem"
 end
