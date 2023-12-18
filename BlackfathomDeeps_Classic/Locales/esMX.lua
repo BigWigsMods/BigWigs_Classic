a8f76be8ffc7dd7dea01b33a4786d8719a30e617
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2023-12-18 10:22:27 -0500

    Update esMX.lua

--- BlackfathomDeeps_Classic/Locales/esMX.lua
+++ BlackfathomDeeps_Classic/Locales/esMX.lua
@@ -6,33 +6,33 @@ end
 
 L = BigWigs:NewBossLocale("Ghamoo-ra Discovery", "esMX")
 if L then
-	--L.bossName = "Ghamoo-ra"
+	L.bossName = "Ghamoo-ra"
 end
 
 L = BigWigs:NewBossLocale("Lady Sarevess Discovery", "esMX")
 if L then
-	--L.bossName = "Lady Sarevess"
+	L.bossName = "Lady Sarevess"
 end
 
 L = BigWigs:NewBossLocale("Gelihast Discovery", "esMX")
 if L then
-	--L.bossName = "Gelihast"
+	L.bossName = "Gelihast"
 end
 
 L = BigWigs:NewBossLocale("Lorgus Jett Discovery", "esMX")
 if L then
-	--L.bossName = "Lorgus Jett"
-	--L.priestess = "Blackfathom Tide Priestess"
-	--L.priestess_short = "Priestess" -- Shortened version of L.priestess (Blackfathom Tide Priestess)
-	--L.murloc = "Blackfathom Murloc"
+	L.bossName = "Lorgus Jett"
+	L.priestess = "Sacerdotisa de las mareas Brazanegra"
+	L.priestess_short = "Sacerdotisa" -- Shortened version of L.priestess (Blackfathom Tide Priestess)
+	L.murloc = "Múrloc Brazanegra"
 end
 
 L = BigWigs:NewBossLocale("Twilight Lord Kelris Discovery", "esMX")
 if L then
-	--L.bossName = "Twilight Lord Kelris"
+	L.bossName = "Señor Crepuscular Kelris"
 end
 
 L = BigWigs:NewBossLocale("Aku'mai Discovery", "esMX")
 if L then
-	--L.bossName = "Aku'mai"
+	L.bossName = "Aku'mai"
 end
