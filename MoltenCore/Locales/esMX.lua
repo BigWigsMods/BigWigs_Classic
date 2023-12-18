c2c7c8135f0854d2a09778abc0ec6539dd0ea2d6
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2023-12-18 12:01:28 -0500

    Update esMX.lua

--- MoltenCore/Locales/esMX.lua
+++ MoltenCore/Locales/esMX.lua
@@ -7,7 +7,7 @@ end
 L = BigWigs:NewBossLocale("Majordomo Executus", "esMX")
 if L then
 	--L.disabletrigger = "Impossible! Stay your attack, mortals... I submit! I submit!"
-	--L.power_next = "Next Power"
+	L.power_next = "Próximo Escudo"
 end
 
 L = BigWigs:NewBossLocale("Ragnaros Classic", "esMX")
@@ -15,14 +15,14 @@ if L then
 	--L.engage_trigger = "NOW FOR YOU,"
 	--L.submerge_trigger = "COME FORTH,"
 
-	--L.submerge = "Submerge"
-	--L.submerge_desc = "Warn for Ragnaros' submerge."
-	--L.submerge_message = "Ragnaros down for 90 sec!"
-	--L.submerge_bar = "Submerge"
+	L.submerge = "Sumersión"
+	L.submerge_desc = "Anuncia la sumersión de Ragnaros."
+	L.submerge_message = "¡Ragnaros se sumerge durante 90 seg!"
+	L.submerge_bar = "Sumersión"
 
-	--L.emerge = "Emerge"
-	--L.emerge_desc = "Warn for Ragnaros' emerge."
-	--L.emerge_message = "Ragnaros emerged, 3 mins until submerge!"
-	--L.emerge_bar = "Emerge"
+	L.emerge = "Emersión"
+	L.emerge_desc = "Anuncia la sumersión de Ragnaros."
+	L.emerge_message = "¡Ragnaros se emergió, 3 minutos hasta que se sumerge!"
+	L.emerge_bar = "Emersión"
 end
 
