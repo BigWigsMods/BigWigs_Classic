6b9f57550e002c2aacd1507043eca87bd82f389b
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2023-12-18 16:17:37 -0500

    Update esES (#23)

--- BlackfathomDeeps_Classic/Locales/esES.lua
+++ BlackfathomDeeps_Classic/Locales/esES.lua
@@ -6,24 +6,24 @@ end
 
 L = BigWigs:NewBossLocale("Ghamoo-ra Discovery", "esES")
 if L then
-	--L.bossName = "Ghamoo-ra"
+	L.bossName = "Ghamoo-ra"
 end
 
 L = BigWigs:NewBossLocale("Lady Sarevess Discovery", "esES")
 if L then
-	--L.bossName = "Lady Sarevess"
+	L.bossName = "Lady Sarevess"
 end
 
 L = BigWigs:NewBossLocale("Gelihast Discovery", "esES")
 if L then
-	--L.bossName = "Gelihast"
+	L.bossName = "Gelihast"
 end
 
 L = BigWigs:NewBossLocale("Lorgus Jett Discovery", "esES")
 if L then
-	--L.bossName = "Lorgus Jett"
+	L.bossName = "Lorgus Jett"
 	L.priestess = "Sacerdotisa de las mareas Brazanegra"
-	--L.priestess_short = "Priestess" -- Shortened version of L.priestess (Blackfathom Tide Priestess)
+	L.priestess_short = "Sacerdotisa" -- Shortened version of L.priestess (Blackfathom Tide Priestess)
 	L.murloc = "Múrloc Brazanegra"
 end
 
@@ -34,5 +34,5 @@ end
 
 L = BigWigs:NewBossLocale("Aku'mai Discovery", "esES")
 if L then
-	--L.bossName = "Aku'mai"
+	L.bossName = "Aku'mai"
 end
