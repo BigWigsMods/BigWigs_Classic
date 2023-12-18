2729ed9fdd8c25dc884f6c00f55d481e798d6db9
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2023-12-18 12:45:15 -0500

    Update esMX (#15)

--- AhnQirajRuins_Classic/Locales/esMX.lua
+++ AhnQirajRuins_Classic/Locales/esMX.lua
@@ -1,19 +1,19 @@
 local L = BigWigs:NewBossLocale("General Rajaxx", "esMX")
 if not L then return end
 if L then
-	--L.wave = "Waves Warnings"
-	--L.wave_desc = "Warn for incoming waves"
+	L.wave = "Anunciar oleadas"
+	L.wave_desc = "Anuncia oleadas entrantes"
 
-	--L.wave_trigger1a = "Kill first, ask questions later... Incoming!"
-	--L.wave_trigger1b = "Remember, Rajaxx, when I said I'd kill you last?"
-	--L.wave_trigger3 = "The time of our retribution is at hand! Let darkness reign in the hearts of our enemies!"
-	--L.wave_trigger4 = "No longer will we wait behind barred doors and walls of stone! No longer will our vengeance be denied! The dragons themselves will tremble before our wrath!"
-	--L.wave_trigger5 = "Fear is for the enemy! Fear and death!"
-	--L.wave_trigger6 = "Staghelm will whimper and beg for his life, just as his whelp of a son did! One thousand years of injustice will end this day!"
-	--L.wave_trigger7 = "Fandral! Your time has come! Go and hide in the Emerald Dream and pray we never find you!"
-	--L.wave_trigger8 = "Impudent fool! I will kill you myself!"
+	L.wave_trigger1a = "Ahí vienen. Intenta que no te maten,"
+	L.wave_trigger1b = "Rajaxx, ¿recuerdas que te dije que serías el último en morir?"
+	L.wave_trigger3 = "¡La hora de las recompensas está próxima! ¡Que la oscuridad reine en el corazón de nuestros enemigos!"
+	L.wave_trigger4 = "¡No volveremos a esperar detrás de rejas y muros de piedra! ¡Nunca más podrán negarnos nuestra venganza! ¡Hasta los dragones temblarán frente a nuestra ira!"
+	L.wave_trigger5 = "¡El miedo es para el enemigo! ¡Miedo y muerte!"
+	L.wave_trigger6 = "¡Corzocelada llorará y rogará por su vida, igual que lo hizo su débil hijo! ¡Hoy acabará un milenio lleno de injusticia!"
+	L.wave_trigger7 = "¡Fandral! ¡Ha llegado tu hora! ¡Ve y escóndete en el Sueño Esmeralda y reza para que nunca te encontremos!"
+	L.wave_trigger8 = "¡Loco imprudente! ¡Acabaré contigo yo mismo!"
 
-	--L.wave_message = "Wave (%d/8)"
+	L.wave_message = "Oleada (%d/8)"
 end
 
 L = BigWigs:NewBossLocale("Buru the Gorger", "esMX")
@@ -25,7 +25,7 @@ end
 L = BigWigs:NewBossLocale("Ossirian the Unscarred", "esMX")
 if L then
 	L.debuff = "Debilidad"
-	--L.debuff_desc = "Warn for various weakness types."
+	L.debuff_desc = "Anunciar debilidades"
 end
 
 L = BigWigs:NewBossLocale("Ruins of Ahn'Qiraj Trash", "esMX")
