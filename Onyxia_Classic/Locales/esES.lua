9f5eba8d17079407aba2fd242d871e98d0ec1467
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2023-12-19 18:40:40 -0500

    Update esES (#30)

--- Onyxia_Classic/Locales/esES.lua
+++ Onyxia_Classic/Locales/esES.lua
@@ -1,8 +1,8 @@
 local L = BigWigs:NewBossLocale("Onyxia", "esES")
 if L then
-	--L.phase1_trigger = "How fortuitous"
-	--L.phase2_trigger = "from above"
-	--L.phase3_trigger = "It seems you'll need another lesson"
+	L.phase1_trigger = "Qué casualidad"
+	L.phase2_trigger = "desde arriba"
+	L.phase3_trigger = "Parece ser que vais a necesitar otra lección"
 
 	L.deep_breath = "Aliento profundo"
 end
