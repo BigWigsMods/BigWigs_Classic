e2e139a0d507cf03609cc20139dd88f1a4f7581e
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2023-12-23 12:56:08 -0500

    Update ptBR (#39)

--- Onyxia_Classic/Locales/ptBR.lua
+++ Onyxia_Classic/Locales/ptBR.lua
@@ -1,8 +1,8 @@
 local L = BigWigs:NewBossLocale("Onyxia", "ptBR")
 if L then
-	--L.phase1_trigger = "How fortuitous"
-	--L.phase2_trigger = "from above"
-	--L.phase3_trigger = "It seems you'll need another lesson"
+	L.phase1_trigger = "Que sorte."
+	L.phase2_trigger = "do alto"
+	L.phase3_trigger = "Parece que vocês vão precisar de outra lição"
 
 	L.deep_breath = "Respiração Profunda"
 end
