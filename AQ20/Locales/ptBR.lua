8e520ca0086259ba25f87734a8f349552b2d91f3
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2023-12-23 12:39:55 -0500

    Update ptBR.lua

--- AQ20/Locales/ptBR.lua
+++ AQ20/Locales/ptBR.lua
@@ -1,14 +1,14 @@
 local L = BigWigs:NewBossLocale("General Rajaxx", "ptBR")
 if not L then return end
 if L then
-	--L.wave_trigger1a = "Kill first, ask questions later... Incoming!"
-	--L.wave_trigger1b = "Remember, Rajaxx, when I said I'd kill you last?"
-	--L.wave_trigger3 = "The time of our retribution is at hand! Let darkness reign in the hearts of our enemies!"
-	--L.wave_trigger4 = "No longer will we wait behind barred doors and walls of stone! No longer will our vengeance be denied! The dragons themselves will tremble before our wrath!"
-	--L.wave_trigger5 = "Fear is for the enemy! Fear and death!"
-	--L.wave_trigger6 = "Staghelm will whimper and beg for his life, just as his whelp of a son did! One thousand years of injustice will end this day!"
-	--L.wave_trigger7 = "Fandral! Your time has come! Go and hide in the Emerald Dream and pray we never find you!"
-	--L.wave_trigger8 = "Impudent fool! I will kill you myself!"
+	L.wave_trigger1a = "Eles estão vindo. Tenta não morrer, sangue-bom."
+	L.wave_trigger1b = "Você se lembra, Rajaxx, quando eu disse que ia te matar por último?"
+	L.wave_trigger3 = "A hora da vingança se aproxima! Deixem as trevas reinar nos corações dos nossos inimigos!"
+	L.wave_trigger4 = "Basta de portas trancadas e muros de pedra nos escondendo. Nossa vingança não será mais negada! Os próprios dragões tremerão diante da nossa ira!
+	L.wave_trigger5 = "Levem o medo ao inimigo! Medo e morte!"
+	L.wave_trigger6 = "Guenelmo vai chorar e suplicar pela própria vida! Exatamente como o moleque do filho dele! Mil anos de injustiça... terminam hoje!"
+	L.wave_trigger7 = "Fandral, sua hora chegou! Vá se esconder no Sonho Esmeralda, e reze para que nós nunca o encontremos!"
+	L.wave_trigger8 = "Tolo insolente! Eu mesmo vou matá-lo!"
 end
 
 L = BigWigs:NewBossLocale("Buru the Gorger", "ptBR")
