9e630fc702c2dccb28629b4ea8716ea29e66a7f7
Funkeh <funkydude@users.noreply.github.com>
2023-12-23 17:54:18 +0000

    Update ptBR.lua

--- AQ20/Locales/ptBR.lua
+++ AQ20/Locales/ptBR.lua
@@ -4,7 +4,7 @@ if L then
 	L.wave_trigger1a = "Eles estão vindo. Tenta não morrer, sangue-bom."
 	L.wave_trigger1b = "Você se lembra, Rajaxx, quando eu disse que ia te matar por último?"
 	L.wave_trigger3 = "A hora da vingança se aproxima! Deixem as trevas reinar nos corações dos nossos inimigos!"
-	L.wave_trigger4 = "Basta de portas trancadas e muros de pedra nos escondendo. Nossa vingança não será mais negada! Os próprios dragões tremerão diante da nossa ira!
+	L.wave_trigger4 = "Basta de portas trancadas e muros de pedra nos escondendo. Nossa vingança não será mais negada! Os próprios dragões tremerão diante da nossa ira!"
 	L.wave_trigger5 = "Levem o medo ao inimigo! Medo e morte!"
 	L.wave_trigger6 = "Guenelmo vai chorar e suplicar pela própria vida! Exatamente como o moleque do filho dele! Mil anos de injustiça... terminam hoje!"
 	L.wave_trigger7 = "Fandral, sua hora chegou! Vá se esconder no Sonho Esmeralda, e reze para que nós nunca o encontremos!"
