092522f504a1a5d5f2f164249f8c37ed63eb0e80
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2023-12-24 09:37:58 -0500

    Update frFR.lua

--- BlackfathomDeeps_Classic/Locales/frFR.lua
+++ BlackfathomDeeps_Classic/Locales/frFR.lua
@@ -1,12 +1,12 @@
 local L = BigWigs:NewBossLocale("Baron Aquanis Discovery", "frFR")
 if not L then return end
 if L then
-	--L.bossName = "Baron Aquanis"
+	L.bossName = "Baron Aquanis"
 end
 
 L = BigWigs:NewBossLocale("Ghamoo-ra Discovery", "frFR")
 if L then
-	--L.bossName = "Ghamoo-ra"
+	L.bossName = "Ghamoo-Ra"
 end
 
 L = BigWigs:NewBossLocale("Lady Sarevess Discovery", "frFR")
@@ -16,15 +16,15 @@ end
 
 L = BigWigs:NewBossLocale("Gelihast Discovery", "frFR")
 if L then
-	--L.bossName = "Gelihast"
+	L.bossName = "Gelihast"
 end
 
 L = BigWigs:NewBossLocale("Lorgus Jett Discovery", "frFR")
 if L then
-	--L.bossName = "Lorgus Jett"
+	L.bossName = "Lorgus Jett"
 	L.priestess = "Prêtresse des marées de Brassenoire"
-	--L.priestess_short = "Priestess" -- Shortened version of L.priestess (Blackfathom Tide Priestess)
-	--L.murloc = "Blackfathom Murloc"
+	L.priestess_short = "Prêtresse" -- Shortened version of L.priestess (Blackfathom Tide Priestess)
+	L.murloc = "Murloc de Brassenoire"
 end
 
 L = BigWigs:NewBossLocale("Twilight Lord Kelris Discovery", "frFR")
@@ -34,5 +34,5 @@ end
 
 L = BigWigs:NewBossLocale("Aku'mai Discovery", "frFR")
 if L then
-	--L.bossName = "Aku'mai"
+	L.bossName = "Aku'mai"
 end
