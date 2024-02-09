bb9c987fe791a85c2e09ca8a8b115ca7cb8c61bd
funkydude <funkydude@users.noreply.github.com>
2024-02-09 13:40:23 +0000

    Gnomeregan_Classic: Update option files

--- Gnomeregan_Classic/Options/Colors.lua
+++ Gnomeregan_Classic/Options/Colors.lua
@@ -1,5 +1,12 @@
 
 BigWigs:AddColors("Grubbis Discovery", {
+	[3019] = {"green","red"},
+	[434941] = "orange",
+	[436027] = "yellow",
+	[436059] = "yellow",
+	[436074] = {"green","red"},
+	[436100] = {"blue","green","orange","yellow"},
+	["adds"] = "cyan",
 	["stages"] = "cyan",
 })
 
@@ -7,11 +14,11 @@ BigWigs:AddColors("Viscous Fallout Discovery", {
 	["stages"] = "cyan",
 })
 
-BigWigs:AddColors("Electrocutioner 6000 Discovery", {
+BigWigs:AddColors("Crowd Pummeler 9-60 Discovery", {
 	["stages"] = "cyan",
 })
 
-BigWigs:AddColors("Crowd Pummeler 9-60 Discovery", {
+BigWigs:AddColors("Electrocutioner 6000 Discovery", {
 	["stages"] = "cyan",
 })
 
--- Gnomeregan_Classic/Options/Sounds.lua
+++ Gnomeregan_Classic/Options/Sounds.lua
@@ -1,14 +1,21 @@
 
 BigWigs:AddSounds("Grubbis Discovery", {
+	[434941] = "info",
+	[436027] = "long",
+	[436059] = "long",
+	[436074] = "alarm",
+	[436100] = "alert",
+	["adds"] = "info",
+	["stages"] = "info",
 })
 
 BigWigs:AddSounds("Viscous Fallout Discovery", {
 })
 
-BigWigs:AddSounds("Electrocutioner 6000 Discovery", {
+BigWigs:AddSounds("Crowd Pummeler 9-60 Discovery", {
 })
 
-BigWigs:AddSounds("Crowd Pummeler 9-60 Discovery", {
+BigWigs:AddSounds("Electrocutioner 6000 Discovery", {
 })
 
 BigWigs:AddSounds("Mechanical Menagerie Discovery", {
