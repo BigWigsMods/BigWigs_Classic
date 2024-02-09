7cc3071b0ca46805ada7fa10d84055999f052703
funkydude <funkydude@users.noreply.github.com>
2024-02-09 16:18:00 +0000

    Gnomeregan_Classic/CrowdPummeler9-60: Initial module

--- Gnomeregan_Classic/CrowdPummeler9-60.lua
+++ Gnomeregan_Classic/CrowdPummeler9-60.lua
@@ -4,7 +4,7 @@
 
 local mod, CL = BigWigs:NewBoss("Crowd Pummeler 9-60 Discovery", 90, -2899)
 if not mod then return end
-mod:RegisterEnableMob(60) -- Crowd Pummeler 9-60 Season of Discovery
+mod:RegisterEnableMob(215728) -- Crowd Pummeler 9-60
 mod:SetEncounterID(2899)
 
 --------------------------------------------------------------------------------
@@ -22,7 +22,12 @@ end
 
 function mod:GetOptions()
 	return {
-		"stages",
+		432423, -- Gnomeregan Smash
+		{432062, "SAY", "ICON", "ME_ONLY_EMPHASIZE"}, -- The Claw!
+		431839, -- Off Balanced
+	},nil,{
+		[432423] = CL.knockback, -- Gnomeregan Smash (Knockback)
+		[432062] = CL.charge, -- The Claw! (Charge)
 	}
 end
 
@@ -31,14 +36,48 @@ function mod:OnRegister()
 end
 
 function mod:OnBossEnable()
-
+	self:Log("SPELL_CAST_SUCCESS", "GnomereganSmash", 432423)
+	self:Log("SPELL_CAST_START", "TheClawStart", 432062)
+	self:Log("SPELL_CAST_SUCCESS", "TheClaw", 432062)
+	self:Log("SPELL_AURA_APPLIED", "OffBalancedApplied", 431839)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "OffBalancedApplied", 431839)
 end
 
 function mod:OnEngage()
-	self:Message("stages", "cyan", CL.stage:format(1), false)
+	self:CDBar(432423, 21, CL.knockback) -- Gnomeregan Smash
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+function mod:GnomereganSmash(args)
+	self:Message(args.spellId, "orange", CL.knockback)
+	self:CDBar(args.spellId, 11.4, CL.knockback)
+	self:PlaySound(args.spellId, "warning")
+end
+
+do
+	local function printTarget(self, name, guid)
+		self:PrimaryIcon(432062, name)
+		self:TargetMessage(432062, "yellow", name, CL.charge)
+		if self:Me(guid) then
+			self:Say(432062, CL.charge, nil, "Charge")
+			self:PlaySound(432062, "alarm", nil, name)
+		end
+	end
+
+	function mod:TheClawStart(args)
+		self:GetUnitTarget(printTarget, 0.1, args.sourceGUID)
+	end
+end
+
+function mod:TheClaw(args)
+	self:PrimaryIcon(args.spellId)
+end
+
+function mod:OffBalancedApplied(args)
+	if self:Me(args.destGUID) then
+		self:StackMessage(args.spellId, "blue", args.destName, args.amount, 3)
+	end
+end
