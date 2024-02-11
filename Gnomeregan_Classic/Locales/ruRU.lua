2186356172104d47a2f4e831a982474228a616af
jammergreek <72779305+jammergreek@users.noreply.github.com>
2024-02-11 18:41:03 +0500

    Update ruRU.lua

--- Gnomeregan_Classic/Locales/ruRU.lua
+++ Gnomeregan_Classic/Locales/ruRU.lua
@@ -1,43 +1,43 @@
 local L = BigWigs:NewBossLocale("Grubbis Discovery", "ruRU")
 if not L then return end
 if L then
-	--L.bossName = "Грязнюк"
-	--L.aoe = "АоЕ урон ближнего боя"
-	--L.cloud = "Облако достигло босса"
-	--L.cone = "\"Frontal\" конус" -- "Frontal" Конус, конус сзади (он пукает)
+	L.bossName = "Грязнюк"
+	L.aoe = "АоЕ урон ближнего боя"
+	L.cloud = "Облако достигло босса"
+	L.cone = "\"Frontal\" конус" -- "Frontal" Cone, it's a rear cone (he's farting)
 end
 
 L = BigWigs:NewBossLocale("Viscous Fallout Discovery", "ruRU")
 if L then
-	--L.bossName = "Липкая муть"
-	--L.desiccated_fallout = "Ожог от радиации" -- NPC ID 216810
+	L.bossName = "Липкая муть"
+	L.desiccated_fallout = "Ожог от радиации" -- NPC ID 216810
 end
 
 L = BigWigs:NewBossLocale("Crowd Pummeler 9-60 Discovery", "ruRU")
 if L then
-	--L.bossName = "Толпогон 9-60"
+	L.bossName = "Толпогон 9-60"
 end
 
 L = BigWigs:NewBossLocale("Electrocutioner 6000 Discovery", "ruRU")
 if L then
-	--L.bossName = "Электрошокер 6000"
+	L.bossName = "Электрошокер 6000"
 end
 
 L = BigWigs:NewBossLocale("Mechanical Menagerie Discovery", "ruRU")
 if L then
-	--L.bossName = "Механический зверинец"
-	--L.attack_buff = "+50% скорость атаки"
-	--L.dont_attack = "Не атакуйте овец"
-	--L.sheep_safe = "Овцу можно безопасно атаковать"
+	L.bossName = "Механический зверинец"
+	L.attack_buff = "+50% скорость атаки"
+	L.dont_attack = "Не атакуйте овец"
+	L.sheep_safe = "Овцу можно безопасно атаковать"
 
-	--L[218242] = "Дракон"
-	--L[218243] = "Овца"
-	--L[218244] = "Белка"
-	--L[218245] = "Курица"
+	L[218242] = "Дракон"
+	L[218243] = "Овца"
+	L[218244] = "Белка"
+	L[218245] = "Курица"
 end
 
 L = BigWigs:NewBossLocale("Mekgineer Thermaplugg Discovery", "ruRU")
 if L then
-	--L.bossName = "Мекжинер Термоштепсель"
-	--L.interruptable = "Прерываемый"
+	L.bossName = "Мекжинер Термоштепсель"
+	L.interruptable = "Прерываемый"
 end
