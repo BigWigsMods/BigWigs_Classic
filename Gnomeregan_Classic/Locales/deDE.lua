3a7e119ac989237d10121f5a9d32bfe5e8583322
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2024-02-11 09:50:11 -0500

    Update deDE (#60)

--- Gnomeregan_Classic/Locales/deDE.lua
+++ Gnomeregan_Classic/Locales/deDE.lua
@@ -1,43 +1,43 @@
 local L = BigWigs:NewBossLocale("Grubbis Discovery", "deDE")
 if not L then return end
 if L then
-	--L.bossName = "Grubbis"
-	--L.aoe = "AoE melee damage"
-	--L.cloud = "A cloud reached the boss"
-	--L.cone = "\"Frontal\" cone" -- "Frontal" Cone, it's a rear cone (he's farting)
+	L.bossName = "Grubbis"
+	L.aoe = "AoE Nahkampfschaden"
+	L.cloud = "Eine Wolke hat den Boss erreicht"
+	L.cone = "\"Frontale\" Kegelattacke" -- "Frontal" Cone, it's a rear cone (he's farting)
 end
 
 L = BigWigs:NewBossLocale("Viscous Fallout Discovery", "deDE")
 if L then
-	--L.bossName = "Viscous Fallout"
+	L.bossName = "Verflüssigte Ablagerung"
 	--L.desiccated_fallout = "Desiccated Fallout" -- NPC ID 216810
 end
 
 L = BigWigs:NewBossLocale("Crowd Pummeler 9-60 Discovery", "deDE")
 if L then
-	--L.bossName = "Crowd Pummeler 9-60"
+	L.bossName = "Meute-Verprügler 9-60"
 end
 
 L = BigWigs:NewBossLocale("Electrocutioner 6000 Discovery", "deDE")
 if L then
-	--L.bossName = "Electrocutioner 6000"
+	L.bossName = "Elektrokutor 6000"
 end
 
 L = BigWigs:NewBossLocale("Mechanical Menagerie Discovery", "deDE")
 if L then
-	--L.bossName = "Mechanical Menagerie"
-	--L.attack_buff = "+50% attack speed"
-	--L.dont_attack = "Don't attack the sheep"
-	--L.sheep_safe = "Sheep is safe to attack"
+	L.bossName = "Mechanische Menagerie"
+	L.attack_buff = "+50% Angriffstempo"
+	L.dont_attack = "Nicht das Schaf angreifen"
+	L.sheep_safe = "Schaf kann sicher angegriffen werden"
 
-	--L[218242] = "Dragon"
-	--L[218243] = "Sheep"
-	--L[218244] = "Squirrel"
-	--L[218245] = "Chicken"
+	L[218242] = "Drache"
+	L[218243] = "Schaf"
+	L[218244] = "Eichhörnchen"
+	L[218245] = "Huhn"
 end
 
 L = BigWigs:NewBossLocale("Mekgineer Thermaplugg Discovery", "deDE")
 if L then
-	--L.bossName = "Mekgineer Thermaplugg"
-	--L.interruptable = "Interruptable"
+	L.bossName = "Robogenieur Thermaplugg"
+	L.interruptable = "Unterbrechbar"
 end
