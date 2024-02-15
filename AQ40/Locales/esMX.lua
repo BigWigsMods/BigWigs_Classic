06f4c221cfb682a2658d4618b751e8faabe39fae
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2024-02-15 09:11:40 -0500

    Update esMX.lua

--- AQ40/Locales/esMX.lua
+++ AQ40/Locales/esMX.lua
@@ -1,70 +1,70 @@
 local L = BigWigs:NewBossLocale("Viscidus", "esMX")
 if not L then return end
 if L then
-	--L.freeze = "Freezing States"
-	--L.freeze_desc = "Warn for the different frozen states."
+	L.freeze = "Estados de congelación"
+	L.freeze_desc = "Anunciar los diferentes estados de congelación."
 
-	--L.freeze_trigger1 = "%s begins to slow!"
-	--L.freeze_trigger2 = "%s is freezing up!"
-	--L.freeze_trigger3 = "%s is frozen solid!"
-	--L.freeze_trigger4 = "%s begins to crack!"
-	--L.freeze_trigger5 = "%s looks ready to shatter!"
+	L.freeze_trigger1 = "%s comienza a ir más despacio!"
+	L.freeze_trigger2 = "%s se está congelando!"
+	L.freeze_trigger3 = "%s no se puede mover!"
+	L.freeze_trigger4 = "%s comienza a desmoronarse!"
+	L.freeze_trigger5 = "%s parece a punto de hacerse añicos!"
 
-	--L.freeze_warn1 = "First freeze phase!"
-	--L.freeze_warn2 = "Second freeze phase!"
-	--L.freeze_warn3 = "Viscidus is frozen!"
-	--L.freeze_warn4 = "Cracking up - keep going!"
-	--L.freeze_warn5 = "Cracking up - almost there!"
-	--L.freeze_warn_melee = "%d melee attacks - %d more to go"
-	--L.freeze_warn_frost = "%d frost attacks - %d more to go"
+	L.freeze_warn1 = "¡Primera fase de congelación!"
+	L.freeze_warn2 = "¡Segunda fase de congelación!"
+	L.freeze_warn3 = "¡Viscidus está congelado!"
+	L.freeze_warn4 = "¡Desmoronándose - sigue adelante!"
+	L.freeze_warn5 = "¡Desmoronándose - casi allí!"
+	L.freeze_warn_melee = "%d ataques cuerpo a cuerpo - faltan %d"
+	L.freeze_warn_frost = "%d ataques de escarcha - faltan %d"
 end
 
 L = BigWigs:NewBossLocale("Ouro", "esMX")
 if L then
-	--L.engage_message = "Ouro engaged! Possible Submerge in 90sec!"
-	--L.possible_submerge_bar = "Possible submerge"
+	L.engage_message = "¡Se entabla combate con Ouro! ¡Sumersión posible en 90 segundos!"
+	L.possible_submerge_bar = "Sumersión posible"
 
-	--L.emerge_message = "Ouro has emerged"
-	--L.emerge_bar = "Emerge"
+	L.emerge_message = "Ouro se ha emergido"
+	L.emerge_bar = "Emersión"
 
-	--L.submerge_message = "Ouro has submerged"
-	--L.submerge_bar = "Submerge"
+	L.submerge_message = "Ouro se ha sumergido"
+	L.submerge_bar = "Sumersión"
 
-	--L.scarab = "Scarab Despawn"
-	--L.scarab_desc = "Warn for Scarab Despawn."
-	--L.scarab_bar = "Scarabs despawn"
+	L.scarab = "Desaparición de escarabajo"
+	L.scarab_desc = "Anuncio para desaparición de escarabajo."
+	L.scarab_bar = "Desaparición de escarabajo"
 end
 
 L = BigWigs:NewBossLocale("C'Thun", "esMX")
 if L then
-	--L.claw_tentacle = "Claw Tentacle"
-	--L.claw_tentacle_desc = "Timers for the claw tentacle."
+	L.claw_tentacle = "Tentáculo Garral"
+	L.claw_tentacle_desc = "Temporizadores para Tentáculo Garral."
 
-	--L.giant_claw_tentacle = "Giant Claw"
-	--L.giant_claw_tentacle_desc = "Timers for the giant claw tentacle."
+	L.giant_claw_tentacle = "Tentáculo garral gigante"
+	L.giant_claw_tentacle_desc = "Temporizadores para Tentáculo garral gigante."
 
-	--L.eye_tentacles = "Eye Tentacles"
-	--L.eye_tentacles_desc = "Timers for the 8 eye tentacles."
+	L.eye_tentacles = "Tentáculo ocular"
+	L.eye_tentacles_desc = "Temporizadores para los 8 Tentáculos oculares."
 
-	--L.giant_eye_tentacle = "Giant Eye"
-	--L.giant_eye_tentacle_desc = "Timers for the giant eye tentacle."
+	L.giant_eye_tentacle = "Tentáculo ocular gigante"
+	L.giant_eye_tentacle_desc = "Temporizadores para Tentáculo ocular gigante."
 
-	--L.weakened_desc = "Warn for weakened state."
-	--L.weakenedtrigger = "%s is weakened!"
+	L.weakened_desc = "Anunciar debilidad."
+	L.weakenedtrigger = "%s está débil!"
 
-	--L.dark_glare_message = "%s: %s (Group %s)" -- Dark Glare: PLAYERNAME (Group 1)
+	L.dark_glare_message = "%s: %s (Grupo %s)" -- Dark Glare: PLAYERNAME (Group 1)
 end
 
 L = BigWigs:NewBossLocale("Ahn'Qiraj Trash", "esMX")
 if L then
-	--L.sentinel = "Anubisath Sentinel" -- NPC 15264
-	--L.brainwasher = "Qiraji Brainwasher" -- NPC 15247
+	L.sentinel = "Centinela Anubisath" -- NPC 15264
+	L.brainwasher = "Lavacerebros qiraji" -- NPC 15247
 	L.defender = "Defensor Anubisath" -- NPC 15277
 	L.crawler = "Reptador de la colmena Vekniss" -- NPC 15240
 
-	--L.target_buffs = "Target Buff Warnings"
-	--L.target_buffs_desc = "When your target is an Anubisath Sentinel, show a warning for what buff it has."
-	--L.target_buffs_message = "Target Buffs: %s"
-	L.detect_magic_missing_message = "Detectar magia is missing from your target"
-	L.detect_magic_warning = "A Mage must cast \124cff71d5ff\124Hspell:2855:0\124h[Detectar magia]\124h\124r on your target for buff warnings to work."
+	L.target_buffs = "Anuncios de beneficios de objetivo"
+	L.target_buffs_desc = "Cuando tu objetivo es un Centinela Anubisath, muestra un anuncio sobre qué beneficio tiene"
+	L.target_buffs_message = "Beneficios de objetivo: %s"
+	L.detect_magic_missing_message = "Falta Detectar magia en tu objetivo"
+	L.detect_magic_warning = "Un mago debe lanzar \124cff71d5ff\124Hspell:2855:0\124h[Detectar magia]\124h\124r en tu objetivo para que funcionen los anuncios de beneficios."
 end
