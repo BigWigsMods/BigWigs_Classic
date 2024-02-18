064037486271abfb9b70ef54eac95bed72ad4373
jammergreek <72779305+jammergreek@users.noreply.github.com>
2024-02-18 11:37:05 +0500

    Update ruRU.lua

--- AQ40/Locales/ruRU.lua
+++ AQ40/Locales/ruRU.lua
@@ -1,60 +1,60 @@
 local L = BigWigs:NewBossLocale("Viscidus", "ruRU")
 if not L then return end
 if L then
-	--L.freeze = "Freezing States"
-	--L.freeze_desc = "Warn for the different frozen states."
+	L.freeze = "Замораживающие состояния"
+	L.freeze_desc = "Предупреждать о различных замороженных состояниях."
 
-	L.freeze_trigger1 = "%s замедлен!!"
-	L.freeze_trigger2 = "%s заморожен!"
-	--L.freeze_trigger3 = "%s заморожен!\ тело!"
+	L.freeze_trigger1 = "%s замедлен!"
+	L.freeze_trigger2 = "%s замерзает!"
+	L.freeze_trigger3 = "%s заморожен!"
 	L.freeze_trigger4 = "%s Начинает трескаться!"
-	L.freeze_trigger5 = "%s Скоро разобьётся на части!!"
+	L.freeze_trigger5 = "%s Скоро разобьётся на части!"
 
-	--L.freeze_warn1 = "First freeze phase!"
-	--L.freeze_warn2 = "Second freeze phase!"
-	--L.freeze_warn3 = "Viscidus is frozen!"
-	--L.freeze_warn4 = "Cracking up - keep going!"
-	--L.freeze_warn5 = "Cracking up - almost there!"
-	--L.freeze_warn_melee = "%d melee attacks - %d more to go"
-	--L.freeze_warn_frost = "%d frost attacks - %d more to go"
+	L.freeze_warn1 = "Первая фаза заморозки!"
+	L.freeze_warn2 = "Вторая фаза заморозки!"
+	L.freeze_warn3 = "Нечистотон заморожен!"
+	L.freeze_warn4 = "Ломается - продолжайте!"
+	L.freeze_warn5 = "Ломается - почти готово!"
+	L.freeze_warn_melee = "%d атак в ближнем бою - осталось %d"
+	L.freeze_warn_frost = "%d ледяных атак - осталось еще %d"
 end
 
 L = BigWigs:NewBossLocale("Ouro", "ruRU")
 if L then
-	--L.engage_message = "Ouro engaged! Possible Submerge in 90sec!"
-	--L.possible_submerge_bar = "Possible submerge"
+	L.engage_message = "Оуро занят! Погружение через 90 секунд!"
+	L.possible_submerge_bar = "Погружение"
 
-	--L.emerge_message = "Ouro has emerged"
-	--L.emerge_bar = "Emerge"
+	L.emerge_message = "Оуро появился"
+	L.emerge_bar = "Появление"
 
-	--L.submerge_message = "Ouro has submerged"
-	--L.submerge_bar = "Submerge"
+	L.submerge_message = "Оуро погрузился"
+	L.submerge_bar = "Погружение"
 
-	--L.scarab = "Scarab Despawn"
-	--L.scarab_desc = "Warn for Scarab Despawn."
-	--L.scarab_bar = "Scarabs despawn"
+	L.scarab = "Скарабей исчез"
+	L.scarab_desc = "Предупреждать об исчезновении скарабея."
+	L.scarab_bar = "Скарабеи исчезли"
 end
 
 L = BigWigs:NewBossLocale("C'Thun", "ruRU")
 if L then
-	--L.claw_tentacle = "Claw Tentacle"
-	--L.claw_tentacle_desc = "Timers for the claw tentacle."
+	L.claw_tentacle = "Когтещупальце"
+	L.claw_tentacle_desc = "Таймеры для Когтещупальца".
 
-	--L.giant_claw_tentacle = "Giant Claw"
-	--L.giant_claw_tentacle_desc = "Timers for the giant claw tentacle."
+	L.giant_claw_tentacle = "Гигантский когтещуп"
+	L.giant_claw_tentacle_desc = "Таймеры для Гигантского когтещупа"
 
-	--L.eye_tentacles = "Eye Tentacles"
-	--L.eye_tentacles_desc = "Timers for the 8 eye tentacles."
+	L.eye_tentacles = "Глазастое щупальце"
+	L.eye_tentacles_desc = "Таймеры для 8 Глазастых щупалец."
 
-	--L.giant_eye_tentacle = "Giant Eye"
-	--L.giant_eye_tentacle_desc = "Timers for the giant eye tentacle."
+	L.giant_eye_tentacle = "Огромный глаз"
+	L.giant_eye_tentacle_desc = "Таймеры для Огромного глазастого щупальца."
 
-	--L.weakened_desc = "Warn for weakened state."
+	L.weakened_desc = "Предупреждение об ослаблении."
 	L.weakenedtrigger = "%s ослаблен"
 
 	--L.dark_glare_message = "%s: %s (Group %s)" -- Dark Glare: PLAYER_NAME (Group 1)
-	--L.stomach = "Stomach"
-	--L.tentacle = "Tentacle (%d)"
+	L.stomach = "Желудок"
+	L.tentacle = "Щупальце (%d)"
 end
 
 L = BigWigs:NewBossLocale("Ahn'Qiraj Trash", "ruRU")
@@ -64,9 +64,9 @@ if L then
 	L.defender = "Анубисат-защитник" -- NPC 15277
 	L.crawler = "Векнисский ядохвост" -- NPC 15240
 
-	--L.target_buffs = "Target Buff Warnings"
-	--L.target_buffs_desc = "When your target is an Anubisath Sentinel, show a warning for what buff it has."
-	--L.target_buffs_message = "Target Buffs: %s"
+	L.target_buffs = "Предупреждения о баффах цели"
+	L.target_buffs_desc = "Когда ваша цель - Анубисат-часовой, показывать предупреждение о том, какой у него бафф."
+	L.target_buffs_message = "Бафф цели: %s"
 	L.detect_magic_missing_message = "Распознавание магии is missing from your target"
 	L.detect_magic_warning = "A Mage must cast \124cff71d5ff\124Hspell:2855:0\124h[Распознавание магии]\124h\124r on your target for buff warnings to work."
 end
