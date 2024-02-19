d3c38b13d0af09d7e734ea546e8a8d2c3b351b3c
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2024-02-19 18:57:04 -0500

    Update koKR.lua

--- World_Classic/Locales/koKR.lua
+++ World_Classic/Locales/koKR.lua
@@ -10,7 +10,7 @@ L = BigWigs:NewBossLocale("Lord Kazzak", "koKR")
 if L then
 	L.bossName = "군주 카자크"
 
-	L.engage_trigger = "군단을 위하여! 킬제덴을 위하여!" --CHECK
+	L.engage_trigger = "불타는 군단과 킬제덴을 위하여!" -- previous phrase 군단을 위하여! 킬제덴을 위하여!, took current phrase from DBM
 
 	L.supreme_mode = "무적 모드"
 end
@@ -19,26 +19,26 @@ L = BigWigs:NewBossLocale("Emeriss", "koKR")
 if L then
 	L.bossName = "에메리스"
 
-	-- L.engage_trigger = "Hope is a DISEASE of the soul! This land shall wither and die!"
+	L.engage_trigger = "희망은 영혼의 병! 이 땅은 말라 죽을 것이다!"
 end
 
 L = BigWigs:NewBossLocale("Lethon", "koKR")
 if L then
 	L.bossName = "레손"
 
-	-- L.engage_trigger = "I can sense the SHADOW on your hearts. There can be no rest for the wicked!"
+	L.engage_trigger = "네놈들의 마음속에서 어둠이 느껴지는구나. 사악한 존재가 쉴 곳은 없다!"
 end
 
 L = BigWigs:NewBossLocale("Taerar", "koKR")
 if L then
 	L.bossName = "타에라"
 
-	-- L.engage_trigger = "Peace is but a fleeting dream! Let the NIGHTMARE reign!"
+	L.engage_trigger = "평화란 부질없는 꿈일 뿐! 이 세상은 악몽이 지배할 것이다!"
 end
 
 L = BigWigs:NewBossLocale("Ysondre", "koKR")
 if L then
 	L.bossName = "이손드레"
 
-	-- L.engage_trigger = "The strands of LIFE have been severed! The Dreamers must be avenged!"
+	L.engage_trigger = "생명의 끈이 끊어졌다! 꿈꾸는 자들이 복수하는 것이 틀림없다!"
 end
