919972d9bda40fd53e1338ced51836ae1469d861
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2024-02-19 18:52:43 -0500

    Update zhTW.lua

--- World_Classic/Locales/zhTW.lua
+++ World_Classic/Locales/zhTW.lua
@@ -19,26 +19,26 @@ L = BigWigs:NewBossLocale("Emeriss", "zhTW")
 if L then
 	L.bossName = "艾莫莉絲"
 
-	-- L.engage_trigger = "Hope is a DISEASE of the soul! This land shall wither and die!"
+	L.engage_trigger = "希望是靈魂染上的疾病!這片土地應該枯竭，從此死氣騰騰！"
 end
 
 L = BigWigs:NewBossLocale("Lethon", "zhTW")
 if L then
 	L.bossName = "雷索"
 
-	-- L.engage_trigger = "I can sense the SHADOW on your hearts. There can be no rest for the wicked!"
+	L.engage_trigger = "我能感受到你內心的陰影。邪惡的侵蝕永遠不會停止！"
 end
 
 L = BigWigs:NewBossLocale("Taerar", "zhTW")
 if L then
 	L.bossName = "泰拉爾"
 
-	-- L.engage_trigger = "Peace is but a fleeting dream! Let the NIGHTMARE reign!"
+	L.engage_trigger = "和平不過是短暫的夢想!讓夢魘統治整個世界吧！"
 end
 
 L = BigWigs:NewBossLocale("Ysondre", "zhTW")
 if L then
 	L.bossName = "伊索德雷"
 
-	-- L.engage_trigger = "The strands of LIFE have been severed! The Dreamers must be avenged!"
+	L.engage_trigger = "生命的希冀已被切斷!夢遊者要展開報復！"
 end
