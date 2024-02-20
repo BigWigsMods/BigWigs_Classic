77ba00689b6869ef8e7edfee9724e65fddffebce
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2024-02-20 07:32:40 -0500

    Update koKR.lua

--- World_Classic/Locales/koKR.lua
+++ World_Classic/Locales/koKR.lua
@@ -10,7 +10,7 @@ L = BigWigs:NewBossLocale("Lord Kazzak", "koKR")
 if L then
 	L.bossName = "군주 카자크"
 
-	L.engage_trigger = "불타는 군단과 킬제덴을 위하여!" -- previous phrase 군단을 위하여! 킬제덴을 위하여!, took current phrase from DBM
+	L.engage_trigger = "불타는 군단과 킬제덴을 위하여!"
 
 	L.supreme_mode = "무적 모드"
 end
