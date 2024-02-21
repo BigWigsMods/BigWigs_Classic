d8ba784852f204272da0b07eaa4f7313a0a9b7bf
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2024-02-21 12:08:05 -0500

    Update ptBR (#127)

--- Gnomeregan_Classic/Locales/ptBR.lua
+++ Gnomeregan_Classic/Locales/ptBR.lua
@@ -5,13 +5,13 @@ if L then
 	L.aoe = "Dano corpo a corpo em área"
 	L.cloud = "Uma nuvem chegou ao chefe"
 	L.cone = "Cone \"frontal\"" -- "Frontal" Cone, it's a rear cone (he's farting)
-	--L.warmup_say_chat_trigger = "Gnomeregan" -- There are still ventilation shafts actively spewing radioactive material throughout Gnomeregan.
+	L.warmup_say_chat_trigger = "Gnomeregan" -- Ainda há dutos de ventilação espalhando material radioativo em Gnomeregan.
 end
 
 L = BigWigs:NewBossLocale("Viscous Fallout Discovery", "ptBR")
 if L then
 	L.bossName = "Precipitação Radioativa Viscosa"
-	--L.desiccated_fallout = "Desiccated Fallout" -- NPC ID 216810
+	L.desiccated_fallout = "Precipitação Desidratada" -- NPC ID 216810
 end
 
 L = BigWigs:NewBossLocale("Crowd Pummeler 9-60 Discovery", "ptBR")
@@ -29,19 +29,19 @@ if L then
 	L.bossName = "Viveiro Mecânico"
 	L.attack_buff = "+50% de velocidade de ataque"
 	L.boss_at_hp = "%s com %d%%" -- BOSS_NAME at 50%
-	--L.red_button = "Red Button"
+	L.red_button = "Botão vermelho"
 
 	L[218242] = "|T134153:0:0:0:0:64:64:4:60:4:60|tDragão"
 	L[218243] = "|T136071:0:0:0:0:64:64:4:60:4:60|tOvelha"
 	L[218244] = "|T133944:0:0:0:0:64:64:4:60:4:60|tEsquilo"
 	L[218245] = "|T135996:0:0:0:0:64:64:4:60:4:60|tFrango"
 
-	--L.run = "Run to the door"
-	--L.run_desc = "Show a message when you defeat this boss to run to the door. This is intended to help you avoid accidentally engaging the next boss."
+	L.run = "Corra até a porta"
+	L.run_desc = "Ao derrotar este chefe, uma mensagem será mostrada instruindo você a correr até a porta. Isso é para ajudá-lo a evitar engajar acidentalmente o próximo chefe."
 end
 
 L = BigWigs:NewBossLocale("Mekgineer Thermaplugg Discovery", "ptBR")
 if L then
 	L.bossName = "Mecangenheiro Termaplugue"
-	--L.red_button = "Red Button"
+	L.red_button = "Botão vermelho"
 end
