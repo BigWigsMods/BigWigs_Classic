d1fea0ca291509d19ddd936cb4c6c084ab7ef927
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-04-05 21:01:55 +0800

    Update zhCN.lua

--- SunkenTemple_Classic/Locales/zhCN.lua
+++ SunkenTemple_Classic/Locales/zhCN.lua
@@ -1,40 +1,40 @@
 local L = BigWigs:NewBossLocale("Atal'alarion Discovery", "zhCN")
 if not L then return end
 if L then
-	--L.bossName = "Atal'alarion"
+	L.bossName = "阿塔拉利恩"
 end
 
 L = BigWigs:NewBossLocale("Festering Rotslime Discovery", "zhCN")
 if L then
-	--L.bossName = "Festering Rotslime"
+	L.bossName = "腐溃烂泥"
 end
 
 L = BigWigs:NewBossLocale("Atal'ai Defenders Discovery", "zhCN")
 if L then
-	--L.bossName = "Atal'ai Defenders"
+	L.bossName = "阿塔莱防御者"
 end
 
 L = BigWigs:NewBossLocale("Dreamscythe and Weaver Discovery", "zhCN")
 if L then
-	--L.bossName = "Dreamscythe and Weaver"
+	L.bossName = "德姆塞卡尔和德拉维沃尔"
 end
 
 L = BigWigs:NewBossLocale("Avatar of Hakkar Discovery", "zhCN")
 if L then
-	--L.bossName = "Avatar of Hakkar"
+	L.bossName = "哈卡的化身"
 end
 
 L = BigWigs:NewBossLocale("Jammal'an and Ogom Discovery", "zhCN")
 if L then
-	--L.bossName = "Jammal'an and Ogom"
+	L.bossName = "预言者迦玛兰和可悲的奥戈姆"
 end
 
 L = BigWigs:NewBossLocale("Morphaz and Hazzas Discovery", "zhCN")
 if L then
-	--L.bossName = "Morphaz and Hazzas"
+	L.bossName = "摩弗拉斯和哈扎斯"
 end
 
 L = BigWigs:NewBossLocale("Shade of Eranikus Discovery", "zhCN")
 if L then
-	--L.bossName = "Shade of Eranikus"
+	L.bossName = "伊兰尼库斯的阴影"
 end
