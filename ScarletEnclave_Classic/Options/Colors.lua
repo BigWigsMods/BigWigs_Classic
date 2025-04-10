4995ac713b3a9dcd39f19eb8c34dda8ef46ed45c
funkydude <funkydude@users.noreply.github.com>
2025-04-10 16:00:16 +0100

    Update option files

--- ScarletEnclave_Classic/Options/Colors.lua
+++ ScarletEnclave_Classic/Options/Colors.lua
@@ -1,32 +1,39 @@
 
 BigWigs:AddColors("Balnazzar", {
-
+	[437472] = "red",
+	[437621] = "yellow",
+	[437834] = "orange",
+	[437984] = {"blue","red"},
+	["stages"] = "cyan",
 })
 
 BigWigs:AddColors("High Commander Beatrix", {
-
+	["stages"] = "cyan",
 })
 
 BigWigs:AddColors("Solistrasza", {
-
+	[1232009] = {"blue","yellow"},
+	["stages"] = "cyan",
 })
 
 BigWigs:AddColors("Alexei the Beastlord", {
-
+	["stages"] = "cyan",
 })
 
 BigWigs:AddColors("Mason the Echo", {
-
+	["stages"] = "cyan",
 })
 
 BigWigs:AddColors("Reborn Council", {
-
+	[1231010] = "blue",
+	["stages"] = "cyan",
 })
 
 BigWigs:AddColors("Lillian Voss", {
-
+	[1232192] = {"blue","yellow"},
+	["stages"] = "cyan",
 })
 
 BigWigs:AddColors("Grand Crusader Caldoran", {
-
+	["stages"] = "cyan",
 })
--- ScarletEnclave_Classic/Options/Sounds.lua
+++ ScarletEnclave_Classic/Options/Sounds.lua
@@ -1,32 +1,26 @@
 
 BigWigs:AddSounds("Balnazzar", {
-
+	[437984] = "warning",
 })
 
 BigWigs:AddSounds("High Commander Beatrix", {
-
 })
 
 BigWigs:AddSounds("Solistrasza", {
-
 })
 
 BigWigs:AddSounds("Alexei the Beastlord", {
-
 })
 
 BigWigs:AddSounds("Mason the Echo", {
-
 })
 
 BigWigs:AddSounds("Reborn Council", {
-
+	[1231010] = "warning",
 })
 
 BigWigs:AddSounds("Lillian Voss", {
-
 })
 
 BigWigs:AddSounds("Grand Crusader Caldoran", {
-
 })
