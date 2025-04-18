cb07c7877f87ae3198e5bc1a53461f11f8d8136c
funkydude <funkydude@users.noreply.github.com>
2025-04-18 19:55:08 +0100

    ScarletEnclave_Classic/Beatrix: Add staging, tank debuff

--- ScarletEnclave_Classic/Beatrix.lua
+++ ScarletEnclave_Classic/Beatrix.lua
@@ -7,6 +7,7 @@ if not mod then return end
 mod:RegisterEnableMob(240812)
 mod:SetEncounterID(3187)
 mod:SetAllowWin(true)
+mod:SetStage(1)
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -24,8 +25,11 @@ end
 function mod:GetOptions()
 	return {
 		{1232390, "ME_ONLY_EMPHASIZE"}, -- Rose's Thorn
+		1232389, -- Unwavering Blade
 		"stages",
 		"berserk",
+	},nil,{
+		[1232389] = CL.tank_debuff, -- Unwavering Blade (Tank Debuff)
 	}
 end
 
@@ -34,11 +38,17 @@ function mod:OnRegister()
 end
 
 function mod:OnBossEnable()
+	self:RegisterMessage("BigWigs_UNIT_TARGET")
+	self:RegisterEvent("NAME_PLATE_UNIT_ADDED")
 	self:Log("SPELL_CAST_SUCCESS", "RosesThorn", 1232390)
 	self:Log("SPELL_AURA_APPLIED", "RosesThornApplied", 1232390)
+	self:Log("SPELL_CAST_SUCCESS", "UnwaveringBlade", 1232389)
+	self:Log("SPELL_AURA_APPLIED", "UnwaveringBladeApplied", 1232389)
+	self:Log("SPELL_AURA_REFRESH", "UnwaveringBladeApplied", 1232389)
 end
 
 function mod:OnEngage()
+	self:SetStage(1)
 	self:Message("stages", "cyan", CL.stage:format(1), false)
 	self:Bar("stages", 120, CL.stage:format(2), "ability_mount_charger")
 	self:Berserk(600, true) -- No engage message
@@ -47,6 +57,26 @@ end
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:BigWigs_UNIT_TARGET(_, mobId, unitTarget)
+	if mobId == 240812 and self:GetStage() ~= 2 and UnitAffectingCombat(unitTarget) then -- High Commander Beatrix
+		self:SetStage(2)
+		local msg = CL.stage:format(2)
+		self:StopBar(msg)
+		self:Message("stages", "cyan", msg, false)
+		self:PlaySound("stages", "long")
+	end
+end
+
+function mod:NAME_PLATE_UNIT_ADDED(_, unit)
+	if self:GetStage() ~= 2 and self:MobId(self:UnitGUID(unit)) == 240812 then -- High Commander Beatrix
+		self:SetStage(2)
+		local msg = CL.stage:format(2)
+		self:StopBar(msg)
+		self:Message("stages", "cyan", msg, false)
+		self:PlaySound("stages", "long")
+	end
+end
 
 do
 	local playerList = {}
@@ -61,4 +91,13 @@ do
 			self:PlaySound(args.spellId, "warning", nil, args.destName)
 		end
 	end
+end
+
+function mod:UnwaveringBlade(args)
+	self:CDBar(args.spellId, 16.1, CL.tank_debuff)
+end
+
+function mod:UnwaveringBladeApplied(args)
+	self:TargetMessage(args.spellId, "purple", args.destName, CL.tank_debuff)
+	self:PlaySound(args.spellId, "alarm", nil, args.destName)
 end
